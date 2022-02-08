e9ce9d46b1f1671644aef034838c78036065e0eb
Arjun Iyer <arjun.iyer@jeane.llc>
2022-02-07 20:21:53 -0500

    template

---
+++ template.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestCase(unittest.TestCase):
+    def test_all(self):
+        cases = []
+        for case,outcome in cases:
+            self.assertEqual(solution(case),outcome)
+        return
+
+
+""" pseudocode
+
+"""
+
+def solution():
+
+    return
+
+if __name__ == "__main__":
+    unittest.main()
+    print(solution())

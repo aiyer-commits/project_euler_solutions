9e7da19f74cc298b9eb719ba7205b89adfc7c73f
Arjun Iyer <arjun.iyer@jeane.llc>
2022-02-16 11:01:51 -0500

    random approach works

--- nine.py
+++ nine.py
@@ -1,19 +1,62 @@
 import unittest
+from random import randint
+from math import prod
 
 """ approach
-a + b + sqrt(a**2 + b**2) = 1000
+random search...
+
+given n
+get a random number less than n -> r0
+subtract r0 from n -> upper_bound
+get a random number less than upper_bound -> r1
+upper_bound - r1 -> r2
+
+test if r0,1,2 are a pythagorean triple...
+continue until you've found one.... 
 
 """
 
 """ pseudocode
-a = 1
-b = n - a - sqrt(a**2 + b**2)
+
+is_pythag(a,b,c)
+return a^2 + b^2 == c^2
+
+
+trial_and_error (n)
+
+while true
+r0 = rand(2 < r < n-2)
+upper_bound = n - r0
+r1 = rand(1,upper_bound)
+r2 = upper_bound - r1
+if r1 != r2 != r3:
+perms = permutations([r0,r1,r2])
+for p in perms
+is_pythag(p)
+return r0*r1*r2
+
+wow, interesting
 
 """
 
 
+def is_pythag(p):
+    return p[0] ** 2 + p[1] ** 2 == p[2] ** 2
+
+
+at_least_one_solution_exists = True
+
+
 def solution(n):
 
+    while at_least_one_solution_exists:
+        r0 = randint(3, n - 3)
+        ub = n - r0
+        r1 = randint(1, ub)
+        r2 = ub - r1
+        if r0 != r1 != r2 and is_pythag([r0, r1, r2]):
+            print([r0, r1, r2])
+            return prod([r0, r1, r2])
 
 
 class TestCase(unittest.TestCase):

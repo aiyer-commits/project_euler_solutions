6779145c1c3b509d0af58c5e0fc9a84e7f8fc0e5
Arjun Iyer <arjun.iyer@jeane.llc>
2022-02-16 10:18:20 -0500

    still not sure how to solve this

--- nine.py
+++ nine.py
@@ -1,51 +1,19 @@
 import unittest
 
 """ approach
-search for values of a,b,c that satisfy the three constraints
+a + b + sqrt(a**2 + b**2) = 1000
+
 """
 
 """ pseudocode
-n = 12
-a_b = 2
-max_ab = n - a_b
-while a_b < max_ab
-c = n - a_b
-a = a_b // 2
-b = a_b - a
-if b != a and a^2 + b^2 == c^2 
-return a*b*c
-a_b += 1
-
-return -1
-
+a = 1
+b = n - a - sqrt(a**2 + b**2)
 
 """
-
-
-def is_pythag_trip(a, b, c):
-    return a ** 2 + b ** 2 == c ** 2
 
 
 def solution(n):
 
-    a_b = 2
-    max_ab = n - a_b
-
-    while a_b < max_ab:
-        c = n - a_b
-        a = a_b // 2
-        b = a_b - a
-        if b == a:
-            while a > 2:
-                a -= 1
-                b += 1
-                if is_pythag_trip(a, b, c):
-                    return a * b * c
-        elif is_pythag_trip(a, b, c):
-            return a * b * c
-        a_b += 1
-
-    return -1
 
 
 class TestCase(unittest.TestCase):

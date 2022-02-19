2c4475cec5e9a1a109c324c6646076901da20ef7
Arjun Iyer <arjun.iyer@jeane.llc>
2022-02-18 19:18:17 -0500

    generator approach

--- seventeen.py
+++ seventeen.py
@@ -24,29 +24,95 @@ range(100,1001) = 37*7 + 71 + (48 + 37)*9 + 3*(900) - 10 + 37*9 + (3 + 8)
 
 this approach *should* work, but its ugly and the assumption here are n is 1000...
 the generator approach might take longer but will be easier to generalize..
+
+this approach above does not produce the answer...
+the generator approach below does work however..
+and should work up to 9999
 """
 
 """ pseudocode
 given a number n in decimal form
-convert to string, count the length.. this set the upper bound of representations.. (thousand, hundred, ten, one)
-given a length... can calculate the number of forms for each length-1, ... , 0.
-len(ones) + len(tens) + len(hundreds)
+
 """
 
+l_20 = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+]
+tens = [
+    "ten",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+]
+andd = "and"
+hundred = "hundred"
+thousand = "thousand"
 
-def solution():
 
-    return
+def rep(m):
+    len_m = len(m)
+    if int(m[0]) == 0 and len_m > 1:
+        return rep(m[1:])
+    if len_m == 4:
+        prefix = l_20[int(m[0]) - 1]
+        return prefix + thousand + rep(m[1:])
+    elif len_m == 3:
+        if int(m[1]) == 0 and int(m[2]) == 0:
+            return l_20[int(m[0]) - 1] + hundred
+        return l_20[int(m[0]) - 1] + hundred + andd + rep(m[1:])
+    elif len_m == 2:
+        if int(m[0]) == 1:
+            return l_20[10 + int(m[1]) - 1]
+        else:
+            return tens[int(m[0]) - 1] + rep(m[1:])
+    elif len_m == 1:
+        if int(m[0]) == 0:
+            return ""
+        return l_20[int(m[0]) - 1]
+    else:
+        return ""
+
+
+def solution(n):
+    letters = ""
+    for m in range(1, n + 1):
+        r = rep(str(m))
+        letters += r
+    return len(letters)
 
 
 class TestCase(unittest.TestCase):
     def test_all(self):
-        cases = []
+        cases = [(5, 19)]
         for case, outcome in cases:
             self.assertEqual(solution(case), outcome)
         return
 
 
 if __name__ == "__main__":
-    # print(solution())
+    print(rep('9999'))
+    print(solution(1000))
     unittest.main()
